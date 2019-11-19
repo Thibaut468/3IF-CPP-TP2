2b5feccb1e8c30548a07e54c549f778389aa0077
Thibaut468 <thibaut.gravey@insa-lyon.fr>
2019-11-19 17:10:01 +0100

    Création ListeTrajets

---
+++ ListeTrajets.cpp
@@ -0,0 +1,86 @@
+/*************************************************************************
+                           ListeTrajets  -  description
+                             -------------------
+    début                : 19 NOVEMBRE 2019
+    copyright            : (C) 2019 par BRANCHEREAU, GRAVEY
+    e-mail               : $EMAIL$
+*************************************************************************/
+
+//---------- Réalisation de la classe <ListeTrajets> (fichier ListeTrajets.cpp) ------------
+
+//---------------------------------------------------------------- INCLUDE
+
+//-------------------------------------------------------- Include système
+using namespace std;
+#include <iostream>
+
+//------------------------------------------------------ Include personnel
+#include "ListeTrajets.h"
+
+//------------------------------------------------------------- Constantes
+
+//----------------------------------------------------------------- PUBLIC
+
+//----------------------------------------------------- Méthodes publiques
+// type ListeTrajets::Méthode ( liste des paramètres )
+// Algorithme :
+//
+//{
+//} //----- Fin de Méthode
+
+Trajet ** ListeTrajets::GetListe() const
+// Algorithme :
+//
+{
+  return liste;
+} //----- Fin de GetListe
+
+//------------------------------------------------- Surcharge d'opérateurs
+
+//-------------------------------------------- Constructeurs - destructeur
+
+ListeTrajets::ListeTrajets ( ) : tailleMax(NBAUGMENTATION), nbTrajets(0)
+// Algorithme :
+//
+{
+#ifdef MAP
+    cout << "Appel au constructeur de <ListeTrajets>" << endl;
+#endif
+
+   liste = new Trajet * [tailleMax];
+} //----- Fin de ListeTrajets
+
+
+ListeTrajets::~ListeTrajets ( )
+// Algorithme :
+//
+{
+#ifdef MAP
+    cout << "Appel au destructeur de <ListeTrajets>" << endl;
+#endif
+
+   delete [] liste;
+} //----- Fin de ~ListeTrajets
+
+
+//------------------------------------------------------------------ PRIVE
+
+//----------------------------------------------------- Méthodes protégées
+
+ void ListeTrajets::augmenterTaille()
+ // Algorithme :
+ //
+{
+   tailleMax+=NBAUGMENTATION;
+
+   Trajet ** tmp = new Trajet * [tailleMax];
+
+   for(int i = 0; i<nbTrajets;i++)
+   {
+      tmp[i]=liste[i];
+   }
+
+   delete [] liste;
+
+   liste=tmp;
+} //----- Fin de GetListe
---
+++ ListeTrajets.h
@@ -0,0 +1,83 @@
+/*************************************************************************
+                           ListeTrajets  -  description
+                             -------------------
+    début                : 19 NOVEMBRE 2019
+    copyright            : (C) 2019 par BRANCHEREAU, GRAVEY
+    e-mail               : $EMAIL$
+*************************************************************************/
+
+//---------- Interface de la classe <ListeTrajets> (fichier ListeTrajets.h) ----------------
+#if ! defined ( LISTETRAJETS_H )
+#define LISTETRAJETS_H
+
+//--------------------------------------------------- Interfaces utilisées
+#include "Trajet.h"
+
+//------------------------------------------------------------- Constantes
+
+const int NBAUGMENTATION = 10;
+
+//------------------------------------------------------------------ Types
+
+//------------------------------------------------------------------------
+// Rôle de la classe <ListeTrajets>
+//
+//
+//------------------------------------------------------------------------
+
+class ListeTrajets
+{
+//----------------------------------------------------------------- PUBLIC
+
+public:
+//----------------------------------------------------- Méthodes publiques
+    // type Méthode ( liste des paramètres );
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+//------------------------------------------------- Surcharge d'opérateurs
+
+//-------------------------------------------- Constructeurs - destructeur
+
+    ListeTrajets ( );
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+    virtual ~ListeTrajets ( );
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+    Trajet ** GetListe() const;
+    // Mode d'emploi :
+     //
+     // Contrat :
+     //
+
+//------------------------------------------------------------------ PRIVE
+
+protected:
+//----------------------------------------------------- Méthodes protégées
+
+   void augmenterTaille();
+   // Mode d'emploi :
+   //
+   // Contrat :
+   //
+
+//----------------------------------------------------- Attributs protégés
+
+   Trajet ** liste;
+   int nbTrajets;
+   int tailleMax;
+
+};
+
+//-------------------------------- Autres définitions dépendantes de <ListeTrajets>
+
+#endif // LISTETRAJETS_H
